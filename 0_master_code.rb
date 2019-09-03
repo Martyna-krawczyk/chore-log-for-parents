4d1d4d900c7eacc4de01773562875341791996fd
Martyna Krawczyk <martynakraw@gmail.com>
2019-09-03 11:48:56 +1000

    original test file with errors

---
+++ 0_master_code.rb
@@ -0,0 +1,107 @@
+chore1 = {
+  task:"Vacuum House",
+  value: 5.00
+}
+chore2 = {
+  task:"Mop Floors",
+  value: 5.00
+}
+chore3 = {
+  task:"Clean Toilet",
+  value: 5.00
+}
+
+chores = [chore1, chore2, chore3]
+
+# balance = 0 
+# p "Current Balance is #{balance}"
+
+loop do 
+  puts "1. View chores"
+  puts "2. Add custom chore to list"
+  puts "3. Remove chore from list"
+  puts "4. View balance"
+  puts "5. Payout" 
+  puts "6. Log Completed chore"
+  puts "7. Exit"
+
+  action = gets.chomp.to_i
+
+  case action
+  when 1
+    for chore in chores
+      puts "----------"
+      puts "Task: #{task[:task]}"
+      puts "Value: #{value[:value]}"
+      puts "----------"
+    end
+    
+  when 2
+    puts "Task:"
+    name = gets.chomp
+    puts "Value:"
+    value = gets.chomp.to_f
+
+    chore = {
+      task: task,
+      value: value,
+    }
+
+    chores.push(chore)
+    puts "Your chore has been added"
+  end
+
+#   when 3
+#         puts "Select the chore to remove:"
+#         task = gets.chomp
+#         chores.each do |chore| 
+#       if chore[:task] == task
+#         puts "Are you sure you want to remove this chore?"
+#         confirm = gets.chomp
+#         if confirm.downcase == 'yes'
+#           index = chores.index(chores)
+#           chores.delete_at(index)
+#           puts "Chore successfully deleted"
+#           break
+#         else
+#           puts "Chore has not been deleted"
+#           break
+#         end
+#       end
+#     end
+
+#   when 4
+#         p balance
+#   end
+#   when 5
+#     loop do    
+#         p "How much would you like to payout to your child??"
+#         print "> "
+#         payout = gets.chomp.to_i
+#         if payout <= balance
+#             balance = balance - payout
+#             system("clear")
+#             p "Your childs balance is now $#{balance}"
+#             transactions.push "Paid out #{withdraw_amount} to child"
+#             break
+#         else withdraw_amount > balance
+#             system("clear")
+#             p "Your child hasn't earned that much!"
+#             break
+#         end
+#     end
+#   end
+# end
+    # when 6
+    #      p "Which chore would you like to mark as completed?"
+    #      print "> "
+    #      deposit_amount = gets.strip.to_i
+    #      balance += deposit_amount
+    #      system("clear")
+    #      p "Your balance is now $#{balance}"
+    #      transactions.push "Deposited #{deposit_amount}"
+    # elsif user_input == "end"
+  when 7
+    break
+  end
+end
---
+++ 1_software_development_plan.md
@@ -0,0 +1,31 @@
+# Software Development Plan
+
+## Statement of Purpose
+The purpose of this app is to assist parents by tracking completed chores, logging new completed chores and accruing the respective dollar value of the chores  so that parents can dispense pocket money to their child in one lump sum. 
+As a parent myself, I would like to be able to track the extra chores the kids do so that I can pay them for their efforts in one lump sum. I feel that the financial insentive to do extra household chores would increase children's interest in helping their parents as well as help them to understand how small amounts of money can add up quickly, turning into a decent sum of money. As the app will have the ability to accrue the total dollar amount earned, this would help with decision making about whether the child should be dispensed cash, or receive money into their bank accounts, thus opening up a dialogue between parents and their children about finance, saving and responsible spending. In a society that is using cash a lot less, I believe this app will also encourage parents to deposit accrued pocket money into their child's bank account instead of dropping a couple of coins into a money box (without the abolity to track savings or spendings).
+The tarket audience for this app are parents of any age, with a child under 15 years old. The parent(user) will be able to select a chore from a pre-set library and mark it as complete, as well as enter a custom chore with its dollar value as chosen by the user.  The user will be able to view a balance of pocket money earned for the completed tasks, as well as the list of completed tasks.
+
+
+## Features
+use of variables and the concept of variable scope
+loops and conditional control structures
+error handling
+### Feature 1 - Adding Custom Chores
+    Rather than limiting the user to the pre-set chore options and their dollar values, the user will be able to add a custom chore and it's respective value which will show up in the menu as an option to mark complete.
+
+### Feature 2 - Accrual of Balance
+    As each chore has been selected, the accrued balance will update in the background so that when the information is required, the user can view their child's accrued balance.
+### Feature 3 - Log a Completed Chore
+    ### Feature 4 - View chores
+    ### Feature 5 - Add custom chore to list
+### Feature 6 - Remove chore from list
+
+### Feature - Payout
+
+### Feature - Exit
+
+## User Experience Outline
+
+
+Flow Diagram
+
