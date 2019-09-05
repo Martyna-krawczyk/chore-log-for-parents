8fc1fe8915ec082000b39ef16bd4e00dfe3324e5
Martyna Krawczyk <martynakraw@gmail.com>
2019-09-05 10:59:47 +1000

    cases now in their own methods and improvement to case 4 puts statement

--- 0_master_code.rb
+++ 0_master_code.rb
@@ -76,12 +76,12 @@ end
 def remove_chore(chores)
     puts "Select the chore to remove from the list"
     print "> "
-    task = gets.chomp.capitalize
+    task = gets.strip.capitalize
     chores.each do |chore| 
     if chore[:task] == task
     puts "Are you sure you want to remove this chore?"
     print "> "
-    confirm = gets.chomp
+    confirm = gets.strip
         if confirm.downcase == 'yes' 
             index = chores.index(chore)
             chores.delete_at(index)
@@ -108,7 +108,30 @@ end
 
 def print_balance(user)
     system("clear")
-    puts "Your Child's Current Balance is $#{user[:total_balance]}0 for completion of the following task(s) #{user[:tasks]}!".colorize(:green)
+    puts "Your Child's Current Balance is $#{user[:total_balance]}0 for completion of the following task(s):"
+    user[:tasks].each do |task|
+        puts task
+    end
+end
+
+def payout_child(user)
+    loop do  
+        puts "Your child's current balance is $#{user[:total_balance]}0".colorize(:green)
+        puts "How much would you like to payout to your child?".colorize(:green)
+        print "> "
+        payout = gets.chomp.to_f
+        if payout <= user[:total_balance]
+            user[:total_balance] = user[:total_balance] - payout
+            system("clear")
+            p "You have paid out $#{payout}0 to your child"
+            p "Your childs balance is now $#{user[:total_balance]}0"
+            break
+        else payout > user[:total_balance]
+            system("clear")
+            puts "Your child hasn't earned that much!".colorize(:red)
+            break
+        end
+    end
 end
 
 loop do 
@@ -122,40 +145,21 @@ loop do
     view_chores_list(chores)
     user_data(chores, user)
 
-    
   when 2
     add_new_chore(chores)
 
-
   when 3
+    # view_chores_list(chores)
     remove_chore(chores)
 
   when 4
     print_balance(user)
 
   when 5
-    loop do  
-        p "Your child's current balance is $#{user[:total_balance]}0"  
-        p "How much would you like to payout to your child?"
-        print "> "
-        payout = gets.chomp.to_f
-        if payout <= user[:total_balance]
-            user[:total_balance] = user[:total_balance] - payout
-            system("clear")
-            p "Your childs balance is now $#{user[:total_balance]}0"
-            p "You have paid out $#{payout}0 to your child"
-            break
-        else payout > user[:total_balance]
-            system("clear")
-            puts "Your child hasn't earned that much!".colorize(:red)
-            break
-        end
-    end
-
+    payout_child(user)
   when 6
     break
-
-end #ends case
-end #ends loop
+end 
+end 
 
 
